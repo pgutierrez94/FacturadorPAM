8f1b88709cb695f6ea2600de0ca7da99e5effc98
Fercho Duque <bull-000@hotmail.com>
2018-12-24 12:01:03 -0500

    First Commit
    To master

---
+++ src/java/Negocio/Utilidad/conexionBD.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio.Utilidad;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author desar
+ */
+public class conexionBD {
+    
+    private Connection conn = null;
+    //private static final String dbURL = "jdbc:sqlserver://DESKTOP-R9PC2V8:1433;DatabaseName=DirOES;integratedSecurity=true;";OES:SQLDirectori0!
+    private static final String dbURL = "jdbc:sqlserver://DESKTOP-R9PC2V8:1433;DatabaseName=DirOES;username=OES;password=pass1234;";
+    
+    public Connection getConnection() throws ClassNotFoundException {
+        try {
+            conn = DriverManager.getConnection(dbURL);
+        } catch (SQLException ex) {
+            System.out.println("Error de BD: " + ex.toString());
+        }
+        return conn;
+    }
+
+    public void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) throws SQLException {
+        if (con != null) {
+            con.close();
+        }
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (rs != null) {
+            rs.close();
+        }
+    }
+    public void closeConnection(Connection con, CallableStatement clstmt, ResultSet rs) throws SQLException {
+        if (con != null) {
+            con.close();
+        }
+        if (clstmt != null) {
+            clstmt.close();
+        }
+        if (rs != null) {
+            rs.close();
+        }
+    }
+}
